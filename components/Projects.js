5f0729fc6da2cb8271dc7970d58f829d283a358d
boukhanouf mohamed <mouh4945@gmail.com>
2023-01-03 23:58:49 +0100

    FIX

--- components/Projects.js
+++ components/Projects.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React  , { useEffect} from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import emblaCarousel from 'embla-carousel';
+
 
 const Projects = () => {
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false } );
+
+    useEffect(() => {
+      if (emblaApi) {
+        // Embla API is ready
+      }
+    }, [emblaApi])
+  
     return (
-        <div className='flex justify-start flex-row  bg-slate-500'>
-              <div>
-              <article className=" w-80 h-80 bg-nav text-center items-center justify-center flex text-3xl  hover:text-white m-2 hover:bg-body/60 hover:border-nav/50 hover:border-2  ">
-                  Project 1
-              </article>
-              </div>
+      <div className="flex justify-start flex-row bg-white " ref={emblaRef}>
+        <div className=" flex justify-start flex-row  bg-black/25">
+        <article className=" w-80 h-80 bg-nav text-center items-center justify-center flex text-3xl  hover:text-white m-2 hover:bg-body/60 hover:border-nav/50 hover:border-2  ">
+            JS
+          </article>
         </div>
-    );
+      </div>
+    )
 }
 
 export default Projects;
