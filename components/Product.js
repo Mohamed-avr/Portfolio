2798ce969753d154e79d757614d6a3cb509cf9ca
boukhanouf mohamed <mouh4945@gmail.com>
2023-01-09 23:56:00 +0100

    ADD

--- components/Product.js
+++ components/Product.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Product = (props) => {
+
+
+     const linkHandling = props.PLink; 
+     console.log(linkHandling);
     return (
         <div className=' text-white border-nav/50 border-2 '>
             <div className=" justify-center relative p-3  pb-0  ">
@@ -10,12 +15,22 @@ const Product = (props) => {
                 
             </div>
             <div className="p-3 ">
-                 <div className="flex  justify-between w-full bg-gray-50 "> 
-                 name : {props.pName}            
-            price : {props.pPrice}
+                 <div className="flex  justify-between w-full items-center  py-2 "> 
+                  <h4 className='font-bold'> {props.pName}    </h4>         
+                    <div>
+                    <span className='px-3 py-2  bg-nav rounded-full mr-2 '> 35</span>
+                    <span className='px-3 py-2 bg-nav rounded-full '> Price : {props.pPrice}</span>
+                    
+                    </div>
                  </div>
 
-            puplicated : {props.pDup}
+                 <div className="flex  justify-between w-full items-center py-2 "> 
+                  <Link href={`https://mostaql.com/portfolio/1153318-%D8%B7%D8%A8%D9%8A%D8%A8%D9%8A`}> 
+                    <button className=" bg-nav rounded-full  w-[100%] h-16 flex justify-center items-center mt-2" > Duplicate now </button>
+                  </Link>
+                 </div>
+
+           
             </div>
           
         </div>
--- pages/Products.js
+++ pages/Products.js
@@ -6,17 +6,20 @@ const Products = () => {
     const products = [
         { 
             'name': 'product one', 
+            'id':2,
             'price': true ? '$30' : 'free', 
             'image':'/ProductsImages/ProductIm_1.jpg',
             'duplicated':'301', 
             'id':1,
+            'link':'https://mostaql.com/portfolio/1153318-%D8%B7%D8%A8%D9%8A%D8%A8%D9%8A'
         } ,
         { 
             'name': 'product one', 
+               'id':2,
             'price': true ? '$30' : 'free', 
             'image':'/ProductsImages/ProductIm_1.jpg',
             'duplicated':'301', 
-            'id':1,
+            'link':'https://mostaql.com/portfolio/1153318-%D8%B7%D8%A8%D9%8A%D8%A8%D9%8A'
         }
     ];
 
@@ -26,7 +29,7 @@ const Products = () => {
         <main className=' mt-40  text-green-600 flex sm:space-y-0 space-y-3 sm:space-x-3 sm:flex-row flex-col'>
           { products.map((e)=> {
            return (
-              <Product key={e.id} pName={e.name} pPrice={e.price} pDup={e.duplicated} pImage={e.image}   />
+              <Product key={e.id} pName={e.name} pPrice={e.price} pDup={e.duplicated} pImage={e.image} pLink={e.link}   />
            )
           }) }
              
